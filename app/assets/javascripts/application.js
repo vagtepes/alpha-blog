8ea5a45692feafda3d72a165e8bfbaa652f8582f
Evangelos <etzintsopoulos@evantzin.co.uk>
2018-04-07 20:06:23 +0100

    Bootstrap installation correction

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 
 //= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require bootstrap-sprockets
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
